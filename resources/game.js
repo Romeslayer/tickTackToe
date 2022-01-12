cc7cf007752ee020e9d6d5186f80520af4abc64e
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-01-12 12:26:22 -0700

    Create game class with empty  board

---
+++ resources/game.js
@@ -0,0 +1,46 @@
+class Game {
+  constructor(player1, player2) {
+    this.player1 = player1;
+    this.player2 = player2;
+    this.board = [
+      {
+        position: 1,
+        value: ''
+      },
+      {
+        position: 2,
+        value: ''
+      },
+      {
+        position: 3,
+        value: ''
+      },
+      {
+        position: 4,
+        value: ''
+      },
+      {
+        position: 5,
+        value: ''
+      },
+      {
+        position: 6,
+        value: ''
+      },
+      {
+        position: 7,
+        value: ''
+      },
+      {
+        position: 8,
+        value: ''
+      },
+      {
+        position: 9,
+        value: ''
+      }
+    ];
+
+  }
+
+}
