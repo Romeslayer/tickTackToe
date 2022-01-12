f1f57b4be5de8ced8f6ce307e86ba947396bebdc
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-01-12 14:09:57 -0700

    Complete functionality for player to take a turn

--- resources/game.js
+++ resources/game.js
@@ -2,6 +2,7 @@ class Game {
   constructor(player1, player2) {
     this.player1 = player1;
     this.player2 = player2;
+    this.currentPlayer = Math.random() < 0.5;
     this.board = [
       {
         position: 1,
@@ -40,7 +41,19 @@ class Game {
         value: ''
       }
     ];
+  }
+//Who goes first?
+// player1 or player2 needs to go first and be displayed on the DOM
+  whosTurn() {
+    return (this.currentPlayer ? `It's ${this.player1.token}'s turn'` : `It's ${this.player2.token}'s turn'`);
+  }
 
+  takeTurn(position) {
+    if(this.currentPlayer) {
+      this.board[position].value = this.player1.token;
+    } else {
+      this.board[position].value = this.player2.token;
+    }
   }
 
 }
