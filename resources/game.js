025e5948d755ae37d51190de0b620202662aeb14
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-01-13 18:20:01 -0700

    Add win conditions for columns and diagonals

--- resources/game.js
+++ resources/game.js
@@ -56,7 +56,6 @@ class Game {
   }
 
   checkBoard() {
-
     if(this.board[0].value === this.board[1].value && this.board[0].value === this.board[2].value) {
       if(this.board[0].value === this.player1.token) {
         this.player1.wins.push(1);
@@ -86,7 +85,76 @@ class Game {
         console.log(`Congratulations ${this.player2.token} you won`);
       }
     }
-    
+
+    if(this.board[0].value === this.board[3].value && this.board[0].value === this.board[6].value) {
+      if(this.board[0].value === this.player1.token) {
+        this.player1.wins.push(1);
+        console.log(`Congratulations ${this.player1.token} you won`);
+      } else {
+        this.player2.wins.push(1);
+        console.log(`Congratulations ${this.player2.token} you won`);
+      }
+    }
+
+    if(this.board[1].value === this.board[4].value && this.board[1].value === this.board[7].value) {
+      if(this.board[1].value === this.player1.token) {
+        this.player1.wins.push(1);
+        console.log(`Congratulations ${this.player1.token} you won`);
+      } else {
+        this.player2.wins.push(1);
+        console.log(`Congratulations ${this.player2.token} you won`);
+      }
+    }
+
+    if(this.board[2].value === this.board[5].value && this.board[2].value === this.board[8].value) {
+      if(this.board[2].value === this.player1.token) {
+        this.player1.wins.push(1);
+        console.log(`Congratulations ${this.player1.token} you won`);
+      } else {
+        this.player2.wins.push(1);
+        console.log(`Congratulations ${this.player2.token} you won`);
+      }
+    }
+
+    if(this.board[0].value === this.board[4].value && this.board[0].value === this.board[8].value) {
+      if(this.board[0].value === this.player1.token) {
+        this.player1.wins.push(1);
+        console.log(`Congratulations ${this.player1.token} you won`);
+      } else {
+        this.player2.wins.push(1);
+        console.log(`Congratulations ${this.player2.token} you won`);
+      }
+    }
+
+    if(this.board[2].value === this.board[4].value && this.board[2].value === this.board[6].value) {
+      if(this.board[2].value === this.player1.token) {
+        this.player1.wins.push(1);
+        console.log(`Congratulations ${this.player1.token} you won`);
+      } else {
+        this.player2.wins.push(1);
+        console.log(`Congratulations ${this.player2.token} you won`);
+      }
+    }
+
+// To get the first row I = 0,1,2
+//  To get the second row I = 3,4,5
+// To get the third row I = 6,7,8
+
+//To get the first column I = 0,3,6
+// To get second column I = 1,4,7
+// To get third column I = 2,5,8
+
+// To get left to right diagonal I = 0,4,8
+//To get right to left diagonal I = 2,4,6
   }
 
+  win(position) {
+    if(this.board[position].value === this.player1.token) {
+      this.player1.wins.push(this.board);
+      console.log(`Congratulations ${this.player1.token} you won`);
+    } else {
+      this.player2.wins.push(this.board);
+      console.log(`Congratulations ${this.player2.token} you won`);
+    }
+  }
 }
