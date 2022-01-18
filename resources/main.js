7ad86ac4684a60dea29b72797f50f057bea22be6
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-01-17 23:29:02 -0700

    Create functionality to display game board

--- resources/main.js
+++ resources/main.js
@@ -2,15 +2,26 @@ var player1 = document.querySelector('.player1-js');
 var player2 = document.querySelector('.player2-js');
 var turnSection = document.querySelector('.player-turn-js');
 var gameBoard = document.querySelector('.game-board-js');
+var boardPositions = document.querySelectorAll('.board-positions-js');
 var ticTacToe = new Game(new Player(1,'X'), new Player(2,'O'));
+window.addEventListener('load', displayPlayer);
+gameBoard.addEventListener('click', playTurn);
 
-gameBoard.addEventListener('click', playTurn)
-
+function displayPlayer() {
+  turnSection.innerHTML = `<h1>${ticTacToe.whosTurn()}<h1>`
+}
 
 function playTurn() {
-  ticTacToe.takeTurn(parseInt(event.target.id))
+  if(event.target.id) {
+    var id = parseInt(event.target.id);
+    ticTacToe.takeTurn(id);
+    displayBoard();
+  }
+  displayPlayer();
 }
 
 function displayBoard() {
-
+  for (var i = 0; i < boardPositions.length; i++) {
+    boardPositions[i].innerHTML = `<p>${ticTacToe.board[i].value}</p>`;
+  }
 }
